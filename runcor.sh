90a83b3de933bacd9c56c4dbcab34c0e912ac012
weirdoglh <lihaog@kth.se>
2024-08-15 09:38:24 +0200

    MMO

--- runcor.sh
+++ runcor.sh
@@ -19,31 +19,32 @@ Nm=2500
 # simulations
 if [ $sim -gt 0 ]
 then
-    # for delay in 1.0 2.0 3.0
-    for delay in 2.0
+    
+    for Nf in 0 25 250
+    # for Nf in 0 25 30 60 150 250
     do
-        # for Nf in 0 25
-        for Nf in 0 25 30 50 90 150 250
+        # for W in -0.1 0.1
+        for W in 0.01 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5
         do
-            # for W in 0.01 0.5
-            for W in 0.01 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5
+            # for B in 0.1
+            for B in 0.1 0.3 0.5 0.7 0.9
             do
-                # for B in 0.5
-                for B in 0.1 0.3 0.5 0.7 0.9
+                for D in 0.0 # 2.0 4.0 6.0 10.0 20.0 40.0 80.0
                 do
                     echo $COUNTER
                     if [ $COUNTER == $THREAD ]
                     then
-                        python runcor.py --Nm $Nm --Nf $Nf --W $W --B $B --epoch $epoch --T $T --D $delay
+                        python runcor.py --Nm $Nm --Nf $Nf --W $W --B $B --epoch $epoch --T $T --D $D
                         wait
                         COUNTER=1
                     else
-                        python runcor.py --Nm $Nm --Nf $Nf --W $W --B $B --epoch $epoch --T $T --D $delay &
+                        python runcor.py --Nm $Nm --Nf $Nf --W $W --B $B --epoch $epoch --T $T --D $D &
                         COUNTER=$(( COUNTER + 1 ))
                     fi
                 done
             done
         done
+        wait
     done
 fi
 
